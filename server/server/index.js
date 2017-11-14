d06593cb9f1e06e80f6ab1084a43c41937678ef3
Samuel Teuber <bub@samweb.org>
2017-11-14 19:14:07 +0100

    Removed unnessary files

--- server/server/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bootstrap = require("../commons/bootstrap/Bootstrap");
-var test = new bootstrap();
-console.log(test.hi('Server'));
-//# sourceMappingURL=index.js.map
